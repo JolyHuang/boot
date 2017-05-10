43b3476828398a4cfe2376687290b5c84f2037a3
JolyHuang <JolyEmail@Gmail.COM>
2017-05-10 15:24:48 +0800

    optimization

--- com.sharingif.cube.spring.boot.parent/com.sharingif.cube.spring.boot.core/src/main/java/com/sharingif/cube/spring/boot/core/CubeConfigure.java
+++ com.sharingif.cube.spring.boot.parent/com.sharingif.cube.spring.boot.core/src/main/java/com/sharingif/cube/spring/boot/core/CubeConfigure.java
@@ -7,7 +7,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.sharingif.cube.core.exception.CubeRuntimeException;
-import com.sharingif.cube.core.util.StringUtils;
 
 /**
  * 加载cube系统文件
