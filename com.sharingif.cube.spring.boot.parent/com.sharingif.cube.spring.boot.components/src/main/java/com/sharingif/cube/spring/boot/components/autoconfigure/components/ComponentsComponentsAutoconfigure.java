5bf815ab5851b00f6cc32a51cc77fa5156b71f3f
JolyHuang <JolyEmail@Gmail.COM>
2017-05-19 19:32:48 +0800

    optimization

---
+++ com.sharingif.cube.spring.boot.parent/com.sharingif.cube.spring.boot.components/src/main/java/com/sharingif/cube/spring/boot/components/autoconfigure/components/ComponentsComponentsAutoconfigure.java
@@ -0,0 +1,20 @@
+package com.sharingif.cube.spring.boot.components.autoconfigure.components;
+
+import com.sharingif.cube.components.sequence.UUIDSequenceGenerator;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by Joly on 2017/5/19.
+ */
+@Configuration
+public class ComponentsComponentsAutoconfigure {
+
+    @Bean(name="uuidSequenceGenerator")
+    public UUIDSequenceGenerator createUUIDSequenceGenerator() {
+        UUIDSequenceGenerator uuidSequenceGenerator = new UUIDSequenceGenerator();
+
+        return uuidSequenceGenerator;
+    }
+
+}
