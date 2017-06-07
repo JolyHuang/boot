28f347c725b39fd725263627aef6be6d13879ceb
JolyHuang <JolyEmail@Gmail.COM>
2017-06-07 13:40:47 +0800

    optimization2

--- com.sharingif.cube.spring.boot.parent/com.sharingif.cube.spring.boot.components/src/main/java/com/sharingif/cube/spring/boot/components/autoconfigure/components/ComponentsComponentsAutoconfigure.java
+++ com.sharingif.cube.spring.boot.parent/com.sharingif.cube.spring.boot.components/src/main/java/com/sharingif/cube/spring/boot/components/autoconfigure/components/ComponentsComponentsAutoconfigure.java
@@ -1,5 +1,7 @@
 package com.sharingif.cube.spring.boot.components.autoconfigure.components;
 
+import com.sharingif.cube.components.sequence.ISequenceHandler;
+import com.sharingif.cube.components.sequence.SequenceHandlerImpl;
 import com.sharingif.cube.components.sequence.UUIDSequenceGenerator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
