ae9c35ab9d35e5d20c14fb7b9e9ec32796f12b4c
JolyHuang <JolyEmail@Gmail.COM>
2017-06-21 20:28:07 +0800

    add security

---
+++ com.sharingif.cube.spring.boot.parent/com.sharingif.cube.spring.boot.security/src/main/java/com/sharingif/cube/spring/boot/security/autoconfigure/components/SecurityComponentsAutoconfigure.java
@@ -0,0 +1,17 @@
+package com.sharingif.cube.spring.boot.security.autoconfigure.components;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * SecurityComponentsAutoconfigure
+ *
+ * @author Joly
+ * @version v1.0
+ * @since v1.0
+ * 2017/6/21 下午8:22
+ */
+@Configuration
+public class SecurityComponentsAutoconfigure {
+
+
+}
