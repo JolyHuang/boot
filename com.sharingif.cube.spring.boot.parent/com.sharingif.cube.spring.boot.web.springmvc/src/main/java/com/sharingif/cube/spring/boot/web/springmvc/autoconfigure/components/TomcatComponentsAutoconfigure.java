aca5f5954b894b742755f42a14be533aca0cea64
JolyHuang <JolyEmail@Gmail.COM>
2017-07-04 19:30:00 +0800

    修改数据源配置字段类型

--- com.sharingif.cube.spring.boot.parent/com.sharingif.cube.spring.boot.web.springmvc/src/main/java/com/sharingif/cube/spring/boot/web/springmvc/autoconfigure/components/TomcatComponentsAutoconfigure.java
+++ com.sharingif.cube.spring.boot.parent/com.sharingif.cube.spring.boot.web.springmvc/src/main/java/com/sharingif/cube/spring/boot/web/springmvc/autoconfigure/components/TomcatComponentsAutoconfigure.java
@@ -61,31 +61,33 @@ public class TomcatComponentsAutoconfigure {
 			resource.setProperty("password", password);
 
 			// 创建连接阶段
-			resource.setProperty("initialSize", dataSourcePoolConfig.getInitialSize());										// 启动时初始化多少连接数
-			resource.setProperty("maxTotal", dataSourcePoolConfig.getMaxTotal());												// 连接池中最多能有多少连接数
-			resource.setProperty("maxWaitMillis", dataSourcePoolConfig.getMaxWaitMillis());									// 请求连接池最大等待时间，单位：毫秒
-			resource.setProperty("maxIdle", dataSourcePoolConfig.getMaxIdle());												// 链接池中最大空闲连接
-			resource.setProperty("minIdle", dataSourcePoolConfig.getMinIdle());												// 链接池中最小空闲连接
+			resource.setProperty("initialSize", String.valueOf(dataSourcePoolConfig.getInitialSize()));											// 启动时初始化多少连接数
+			resource.setProperty("maxTotal", String.valueOf(dataSourcePoolConfig.getMaxTotal()));												// 连接池中最多能有多少连接数
+			resource.setProperty("maxWaitMillis", String.valueOf(dataSourcePoolConfig.getMaxWaitMillis()));										// 请求连接池最大等待时间，单位：毫秒
+			resource.setProperty("maxIdle", String.valueOf(dataSourcePoolConfig.getMaxIdle()));													// 链接池中最大空闲连接
+			resource.setProperty("minIdle", String.valueOf(dataSourcePoolConfig.getMinIdle()));													// 链接池中最小空闲连接
 
 			// 程序关闭连接阶段,归还连接到连接池
-			resource.setProperty("removeAbandonedOnBorrow", dataSourcePoolConfig.getRemoveAbandonedOnBorrow());				// 是否清理被遗弃的连接，和removeAbandonedTimeout一起使用
-			resource.setProperty("removeAbandonedTimeout", dataSourcePoolConfig.getRemoveAbandonedTimeout());					// 清理被遗弃的连接等待时间，单位：秒
+			resource.setProperty("removeAbandonedOnBorrow", String.valueOf(dataSourcePoolConfig.isRemoveAbandonedOnBorrow()));					// 是否清理被遗弃的连接，和removeAbandonedTimeout一起使用
+			resource.setProperty("removeAbandonedTimeout", String.valueOf(dataSourcePoolConfig.getRemoveAbandonedTimeout()));					// 清理被遗弃的连接等待时间，单位：秒
 
 			// 空闭连接处理阶段,空闭连接断开从连接池中清处掉
-			resource.setProperty("timeBetweenEvictionRunsMillis", dataSourcePoolConfig.getTimeBetweenEvictionRunsMillis()); 	// 多长时间检查一次连接池中空闲的连接,单位：毫秒
-			resource.setProperty("minEvictableIdleTimeMillis", dataSourcePoolConfig.getMinEvictableIdleTimeMillis());  		// 空闲时间超过多少时间的连接断开,直到连接池中的连接数到minIdle为止，单位：毫秒
-			resource.setProperty("numTestsPerEvictionRun", dataSourcePoolConfig.getNumTestsPerEvictionRun());					// 每次检查连接的数目，建议设置和maxActive一样大
-			resource.setProperty("logAbandoned", dataSourcePoolConfig.getLogAbandoned());										// 接池收回空闲的活动连接时是否打印消息
+			resource.setProperty("timeBetweenEvictionRunsMillis", String.valueOf(dataSourcePoolConfig.getTimeBetweenEvictionRunsMillis())); 	// 多长时间检查一次连接池中空闲的连接,单位：毫秒
+			resource.setProperty("minEvictableIdleTimeMillis", String.valueOf(dataSourcePoolConfig.getMinEvictableIdleTimeMillis()));  			// 空闲时间超过多少时间的连接断开,直到连接池中的连接数到minIdle为止，单位：毫秒
+			resource.setProperty("numTestsPerEvictionRun", String.valueOf(dataSourcePoolConfig.getNumTestsPerEvictionRun()));					// 每次检查连接的数目，建议设置和maxActive一样大
+			resource.setProperty("logAbandoned", String.valueOf(dataSourcePoolConfig.isLogAbandoned()));										// 接池收回空闲的活动连接时是否打印消息
 
 			// 连接验证阶段,防止mysql主动断开连接
-			resource.setProperty("testOnBorrow", dataSourcePoolConfig.getTestOnBorrow());										// 取时检验
-			resource.setProperty("testOnReturn", dataSourcePoolConfig.getTestOnReturn());										// 归还检验
-			resource.setProperty("testWhileIdle", dataSourcePoolConfig.getTestWhileIdle());									// 空闲检验
-			resource.setProperty("validationQuery", dataSourcePoolConfig.getValidationQuery());								// 验证sql
+			resource.setProperty("testOnBorrow", String.valueOf(dataSourcePoolConfig.isTestOnBorrow()));										// 取时检验
+			resource.setProperty("testOnReturn", String.valueOf(dataSourcePoolConfig.isTestOnReturn()));										// 归还检验
+			resource.setProperty("testWhileIdle", String.valueOf(dataSourcePoolConfig.isTestWhileIdle()));										// 空闲检验
+			resource.setProperty("validationQuery", dataSourcePoolConfig.getValidationQuery());													// 验证sql
 
 			context.getNamingResources().addResource(resource);
 		}
 
 	}
+
+
 
 }
